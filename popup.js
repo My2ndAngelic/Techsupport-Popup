b7fdf74a17bf18c57b85b5c72f00f2046db18eb8
Daniel Gray <dg@danielgray.me>
2016-11-05 20:34:48 +0000

    Added the script files

---
+++ popup.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Daniel on 05/11/2016 www.danielgray.me
+ */
+
+var looper = 1
+while (looper = 1) {
+    window.alert("Your dextop is infected! \n Please call technical support immediately \n (This is an example of a fake scam popup)")
+}
